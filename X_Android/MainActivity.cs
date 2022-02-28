7c6935295fc3a16e3d5a7300cdf625814cc20184
KlaasHagge <klaash@ppedv.de>
2022-02-28 14:59:52 +0100

    xamarin.android

--- X_Android/MainActivity.cs
+++ X_Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace X_Android
@@ -8,18 +10,50 @@ namespace X_Android
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public EditText Edt_Input { get; set; }
+        public Button Btn_Ok { get; set; }
+        public Button Btn_Google { get; set; }
+        public Button Btn_ShowPicture { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Edt_Input = FindViewById<EditText>(Resource.Id.activity_main_Edt_Input);
+            Btn_Ok = FindViewById<Button>(Resource.Id.activity_main_Btn_Ok);
+
+            //Btn_Ok.Click += Btn_Ok_Click;
+
+            Btn_Ok.Click += (s,e) => Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
+
+
+            Btn_Google = FindViewById<Button>(Resource.Id.activity_main_Btn_Google);
+
+            Intent implizieterIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("https://www.google.de"));
+
+            Btn_Google.Click += (s, e) => StartActivity(implizieterIntent);
+
+
+            Btn_ShowPicture = FindViewById<Button>(Resource.Id.activity_main_Btn_ShowPicture);
+
+            Intent explizieterIntent = new Intent(this, typeof(ShowPictureActivity));
+
+            Btn_ShowPicture.Click += (s, e) => StartActivity(explizieterIntent);
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void Btn_Ok_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, $"Ihre gewählte Zahl ist {Edt_Input.Text}.", ToastLength.Long).Show();
         }
     }
 }
